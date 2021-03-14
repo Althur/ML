01996d2b362363b65852f16fd27514d3e62e654c
Althur <Althur@users.noreply.github.com>
2021-03-14 18:33:45 -0300

    Unfinnished tutorial

---
+++ google_recepies/recipe_6.py
@@ -0,0 +1,5 @@
+# Here we will build a classifier with TensorFlow for Poets
+# the goal is to train a classifier to tell the difference between 5 types of Flowers
+# Not necessary
+# Best off working on the tutorial: https://www.udacity.com/course/intro-to-tensorflow-for-deep-learning--ud187
+# This way you can get some "Horas Complementares from Insper"
